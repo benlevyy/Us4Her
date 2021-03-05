10e9fc9eacdba48f7cb8a956116f652ffaa6ea89
Ben Levy <thebenlevy@gmail.com>
2021-03-04 18:58:25 -0800

    Initial Commit

---
+++ UsForHer/UsForHerApp.swift
@@ -0,0 +1,17 @@
+//
+//  UsForHerApp.swift
+//  UsForHer
+//
+//  Created by Ben Levy on 3/4/21.
+//
+
+import SwiftUI
+
+@main
+struct UsForHerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
